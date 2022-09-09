defcdff6ce2a79184abe6fc98c5e81c354a67b8d
iconart <iconart34@gmail.com>
2022-09-09 07:21:08 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print digig numbers of base 10
+ * Return: is 0 if successful
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	{
+		printf("%d", digit);
+	}
+	return (0);
+}
