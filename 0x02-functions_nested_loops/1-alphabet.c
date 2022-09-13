11c658033a21d98382595719627e3ec775e52e46
iconart <iconart34@gmail.com>
2022-09-13 22:59:07 +0000

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,17 @@
 
 /**
  * print-alphabet - Entry point
- *
+ * Description: print lowercase alphabet
  * Return: always 0
  */
 
 void print_alphabet(void)
 {
 	char letter;
-	for ( letter = 'a'; letter <= 'z'; letter++)
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
 	}
 	putchar(letter);
-};
+}
