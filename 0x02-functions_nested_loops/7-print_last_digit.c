1e97f9616ce72827b498523174534f61140515d7
iconart <iconart34@gmail.com>
2022-09-14 04:14:07 +0000

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
-#include 'main.h'
+#include "main.h"
 
 /**
  * print_last_digit - print last digit
@@ -6,13 +6,13 @@
  * Return: value of the last digit
  */
 
-int print_last digit(int n)
+int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
 	if (last_digit < 0)
 		last_digit *= -1;
 
-	-putchar(last_digit + '0');
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
