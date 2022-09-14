bdba65a76406e2e97e39277d24354e7ca2310ff2
iconart <iconart34@gmail.com>
2022-09-14 04:02:27 +0000

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - function to check
+ * @n: the character n should be checked
+ * Return: +ve of -ve
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
