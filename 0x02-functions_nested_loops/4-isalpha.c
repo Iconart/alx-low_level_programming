ad2431e685be1c5ce0e14d97f1da1e9985def010
iconart <iconart34@gmail.com>
2022-09-14 03:00:13 +0000

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - function to return the c lower
+ *
+ * @c: the character c should be checked
+ *
+ * Return: 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	if (c == 'c' && c == "C")
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
