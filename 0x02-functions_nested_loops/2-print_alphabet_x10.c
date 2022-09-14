b08b63e8f01391c2aa912a06b802733176345f10
iconart <iconart34@gmail.com>
2022-09-14 00:20:58 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function to print alphabet x10
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		char letter;
+
+		for (letter = 'a'; letter <= 'z' letter++)
+		{
+			putchar(letter);
+		}
+		putchar('\n');
+	}
+}
