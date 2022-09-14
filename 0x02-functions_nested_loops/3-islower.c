0d60466c975d6cc5e60ee3bb94746c359fff1f70
iconart <iconart34@gmail.com>
2022-09-14 02:28:59 +0000

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - function to check for lowercase
+ *
+ * Return: 0 Or 1
+ *
+ */
+
+int _islower(int c)
+{
+	if(c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
