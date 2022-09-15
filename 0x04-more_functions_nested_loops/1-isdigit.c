456b112d8f0943d7bb473f9b18ab1d16b960115f
iconart <iconart34@gmail.com>
2022-09-15 17:47:56 +0000

    check of digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - function to check for digit
+ * @c: check the character c if it is a digit
+ * Return: return 1 if c is a digit else return 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
