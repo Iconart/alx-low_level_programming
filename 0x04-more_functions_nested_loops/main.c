c2d679c9fc2f8e4e5ba44136ad5997e25545b918
iconart <iconart34@gmail.com>
2022-09-17 04:47:11 +0000

    oke

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
 #include "main.h"
-
+void print_square(int size);
 int main(void)
 {
-	more_numbers();
+	print_square(2);
+	print_square(10);
+	print_square(0);
 	return (0);
 }
