77d070b7e06f87871f2c60d8a95434ce0a2c8392
iconart <iconart34@gmail.com>
2022-09-19 22:53:26 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function the check for the length of a string
+ * @s: string s should be checked
+ * Return: void
+ */
+
+int _strlen(int *s)
+{
+	int count;
+	int length;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		length = count;
+	}
+	return (length);
+}
