94f9a8acd035087f4e6ce50b71bb8d295c48bbfc
iconart <iconart34@gmail.com>
2022-09-19 21:06:13 +0000

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function change the value in a variable
+ * @n: The character should be checked
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
