88d3cedea8b55396e8b3ea3e5dbe1860b4f6a29b
iconart <iconart34@gmail.com>
2022-09-20 12:54:51 +0000

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,6 +13,7 @@ void rev_string(char *s)
 	int i = 0;
 	int *loop = &i;
 	int j;
+	char *result;
 
 	while (s[i] != '\0')
 		i++;
@@ -21,7 +22,7 @@ void rev_string(char *s)
 	{
 		if (s[j] != '\0')
 		{
-			*s = s[j];
+			result = s[j];
 		}
 	}
 }
