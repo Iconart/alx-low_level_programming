cb28d6161b342a8d3f2c1bbded6f88a48e6572d2
iconart <iconart34@gmail.com>
2022-09-20 08:26:07 +0000

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _puts - funtion to print a string end with a new line
+ *
+ * @str: check the string str
+ *
+ * Return: void;
+ */
+
+void _puts(char *str)
+{
+	int loop;
+
+	for (loop = 0; loop < 1000; i++)
+	{
+		if (str[loop] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		else
+		{
+			putchar(str[loop]);
+		}
+	}
+}
