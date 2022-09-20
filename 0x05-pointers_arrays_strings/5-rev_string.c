8138a445445861c806e62c257a73a6652e69590e
iconart <iconart34@gmail.com>
2022-09-20 10:49:32 +0000

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - function to reverse a string
+ *
+ * @s: the character s should be checked
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int *loop = &i;
+	int j;
+
+	while (s[i] != '\0')
+		i++;
+
+	for (j = *loop; j >= 0; j++)
+	{
+		if (s[j] != '\0')
+		{
+			putchar(s[j]);
+		}
+	}
+}
