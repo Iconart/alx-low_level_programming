8067d9990af1d07fdb9a085b266e3ea8461b37f2
iconart <iconart34@gmail.com>
2022-09-20 10:32:07 +0000

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,14 +11,14 @@
 void print_rev(char *s)
 {
 	int i = 0;
+	int *loop = &i;
+	int j;
 
 	while (s[i] != '\0')
 		i++;
 
-	int *loop = &i;
-	int j = *loop;
 
-	for (j; j >= 0; j--)
+	for (j = *loop; j >= 0; j--)
 	{
 		if (s[j] != '\0')
 			putchar(s[j]);
