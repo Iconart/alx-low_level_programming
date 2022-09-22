f0fdb3bca501f253df144c3c4bb55bc6c205fa8d
iconart <iconart34@gmail.com>
2022-09-22 13:52:57 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int lenDest = 0;
-	int lenSrc = 0;
+	int len1 = 0;
+	int len2 = 0;
 	int i;
 
-	while (dest[lenDest] != '\0')
-	{
-		lenDest++;
-	}
-	while (src[lenSrc] != '\0')
-	{
-		lenSrc++;
-	}
+	while (dest[len1] != '\0')
+		len1++;
+	while (src[len2] != '\0')
+		len2++;
 
-	for (i = 0; i <= lenScr; i++)
-	{
-		dest[lenDest + i] = src[i];
-	}
-	dest[lenDest + lenSrc + 1] = '\0';
+	for (i = 0; i <= len2; i++)
+		dest[len1 + i] = src[i];
 	return (dest);
 }
