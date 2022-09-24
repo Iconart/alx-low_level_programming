9ff46e4b240c9e3348ba5884ffdf7e5db65cbfd3
iconart <iconart34@gmail.com>
2022-09-24 00:05:18 +0000

    7-leet.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,7 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *str);
+char *cap_string(char *str);
+char *leet(char *str);
 
 #endif
