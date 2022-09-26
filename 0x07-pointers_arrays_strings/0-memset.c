c2f5c74d51774b65bf76c1e8dbc0bcbc2fbf42a5
iconart <iconart34@gmail.com>
2022-09-26 17:27:33 +0000

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - function to store a specific value to a
+ * certain number of address pointing to
+ * @s: the memory addresses
+ * @b: special character to be stored
+ * @n: numbers of address to store value b into
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
