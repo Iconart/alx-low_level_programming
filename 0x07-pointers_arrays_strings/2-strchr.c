b3711d8c63e38340389c12461aadc472fec22a34
iconart <iconart34@gmail.com>
2022-09-27 12:09:07 +0000

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strchr(char *s, char c);
+ * @s: this string should be checked
+ * @c: this character should be checked
+ * Return: c
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+	char result;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == c)
+		{
+			result = c;
+		}
+		else
+		{
+			result = '\0';
+		}
+	}
+	return result;
+}
