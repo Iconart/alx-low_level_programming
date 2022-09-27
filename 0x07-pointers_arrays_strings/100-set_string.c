73a0cd7e058334293ec9e0930d9b3a8236cf6c89
iconart <iconart34@gmail.com>
2022-09-27 19:36:06 +0000

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - function to set string
+ * @s: the character should be checked
+ * @to: the character should be checked
+ * Return: void;
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
