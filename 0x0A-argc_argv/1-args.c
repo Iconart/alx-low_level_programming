a93f645974b3ae8615815d66fdeb3659571af3ae
iconart <iconart34@gmail.com>
2022-10-01 12:30:50 +0000

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc - the numbers of argument passed
+ * @argv - the array of string passed
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
