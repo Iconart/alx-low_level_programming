94b57f82055898695f0359f5e21cdac090f33e17
iconart <iconart34@gmail.com>
2022-10-01 13:48:48 +0000

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * @argc: the number of argument passed
+ * @argv: the array of string passed
+ * Return: 0 if success else 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int count, mul = 0;
+
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			if (isdigit(*argv[count]))
+			{
+				mul += atoi(argv[count]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
+}
