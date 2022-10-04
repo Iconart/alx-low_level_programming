74aa21272a403daf8a3096e01e8c61292a823455
iconart <iconart34@gmail.com>
2022-10-04 10:57:30 +0000

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,7 +15,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
-		sizeStr += i;
+		sizeStr++;
 
 	pointer = malloc(sizeof(*str) * sizeStr);
 	if (pointer == NULL)
