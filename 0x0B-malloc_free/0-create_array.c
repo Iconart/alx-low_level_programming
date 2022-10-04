d6edd9b89f3f04b9aebc36c671a1fd257cb87bb7
iconart <iconart34@gmail.com>
2022-10-04 09:25:15 +0000

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_array - function to allocate array space
+ * @size: the number of items added to the array
+ * @c: the data type
+ * Return: return 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int i;
+
+	if (size == 0)
+		return (NULL);
+
+	s = malloc(sizeof(char) * size);
+	if (s == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
+	return (s);
+
+}
