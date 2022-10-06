1ac90b7a8bf3ec1e7c022ebbfcf0163aa566f7ed
iconart <iconart34@gmail.com>
2022-10-06 09:28:28 +0000

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function to allocate memory
+ * @b: the number memory to be allocated
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *pointer;
+
+	pointer = malloc(b);
+}
