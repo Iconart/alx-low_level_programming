0d48b540bd42045326ec497760fa917519d6fa77
iconart <iconart34@gmail.com>
2022-10-06 23:20:47 +0000

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.c"
+
+/**
+ * _calloc - function to allocate memory for an array
+ * @nmeb: the number of element passed into the array
+ * @size: the size of byte of data type
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmeb, unsigned int size)
+{
+	char *pointer;
+
+	if (nmeb == 0 || size == 0)
+		return (NULL);
+	pointer = malloc(nmeb * size);
+
+	if (pointer == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmeb; i++)
+		pointer[i] = 0;
+	return (pointer);
+}
