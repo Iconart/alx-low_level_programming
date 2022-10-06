6afe29de9e1f5a488062282a9a9dc2f3a5668931
iconart <iconart34@gmail.com>
2022-10-06 20:27:26 +0000

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		sum = s1Len + s2Len;
 	else
 		sum = s1Len + n;
-	pointer = malloc(sizeof(char *) * sum);
+	pointer = malloc(sizeof(*s1) * sum);
 	if (pointer == NULL)
 		return (NULL);
 	for (i = 0; i < s1Len; i++)
