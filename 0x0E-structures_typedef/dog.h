2a292e4061932031d6c58c7468e2c250831a2ef3
Iconart <iconart34@gmail.com>
2022-10-10 22:50:49 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,11 +6,12 @@
  * @age: the age of the dog
  * @owner: who owns the dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
 #endif
