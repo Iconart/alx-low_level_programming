57b8182d95de9841dbfb5a889d5a2d4b8e858b10
Iconart <iconart34@gmail.com>
2022-10-10 13:20:49 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog data type 
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: who owns the dog
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+#endif
