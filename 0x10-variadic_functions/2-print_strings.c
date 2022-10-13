7edac6f748185a8dcf10e4f48dc4fbad60b47b0d
Iconart <iconart34@gmail.com>
2022-10-13 20:28:41 +0000

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		return;
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(point, char *) == NULL)
+		if (point == NULL)
 			printf("(nil)");
 		else
 			printf("%s", va_arg(point, char *));
